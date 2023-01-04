baf92522867f279d2af9cf6629ad0dab1389496d
Biniam Kassahun <george.beng@gmail.com>
2023-01-04 13:06:04 +0300

    update

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [ "$2" = "--enable-interceptor-client" ]; then
+  echo 'Starting interceptord'
+  rm -rf /tmp/interceptord.lock
+  interceptord.py &
+fi
+
+echo 'Starting jasmind'
+exec "$@"
