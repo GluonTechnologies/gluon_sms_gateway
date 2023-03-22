d7de8f41766ec2c4e0c296ed678fb098a4cdae0c
Biniam Kassahun <george.beng@gmail.com>
2023-03-22 09:50:09 +0300

    add ben interceptor

--- ben.py
+++ ben.py
@@ -1,6 +1,3 @@
-import logging
-
-
 def modify(message):
     lookup_json = {
         'google': 'G-o-o-g-l-e',
@@ -11,17 +8,6 @@ def modify(message):
         if word.lower() in list(lookup_json.keys()):
             message = message.replace(word, lookup_json[word.lower()])
     return bytes(message, 'utf-8')
-
-# Set logger
-logger = logging.getLogger('logging-example')
-if len(logger.handlers) != 1:
-    hdlr = logging.FileHandler('/opt/log.log')
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    hdlr.setFormatter(formatter)
-    logger.addHandler(hdlr)
-    logger.setLevel(logging.DEBUG)
-
-logger.info('Got pdu: %s' % routable.pdu.params)
 
 routable.pdu.params['source_addr'] = '9543'
 routable.pdu.params['short_message'] = modify(routable.pdu.params['short_message'])
