f73d6a7c99ed2a8fa35954e9e3471e2da3da8778
Biniam Kassahun <george.beng@gmail.com>
2023-06-20 06:53:13 +0300

    modify interceptor

--- inter/interceptor.py
+++ inter/interceptor.py
@@ -1,13 +1,21 @@
-import re
-
-
 def modify(msg):
-    pattern = r"\b\d{2,9}\b"
-    matches = re.findall(pattern, str(msg.decode()))
+    msg_str = msg.decode()
+    matches = []
+    current_match = ""
+    for char in msg_str:
+        if char.isdigit():
+            current_match += char
+        else:
+            if current_match:
+                matches.append(current_match)
+                current_match = ""
+    if current_match:
+        matches.append(current_match)
+
     if matches:
         return "Your verification code: " + matches[0]
     else:
-        return str(msg.decode())
+        return msg_str
 
 routable.pdu.params['source_addr'] = '9481'
 routable.pdu.params['short_message'] = modify(routable.pdu.params['short_message'])
